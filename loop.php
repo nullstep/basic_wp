c5369f15a5ee1ba1d683f82f2661df5603ebb0b2
nullstep <me@nullstep.com>
2021-06-07 14:24:41 +0100

    fix stupid error

--- loop.php
+++ loop.php
@@ -2,7 +2,6 @@
 				<article>
 					<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 					<p class="post-datetime"><span class="post-date"><?php the_time(get_option('date_format')); ?></span> - <span class="post-time"><?php the_time(); ?></span></p>
-<?php endif; ?>
 					<p class="post-excerpt">
 <?php if (has_post_thumbnail()) : ?>
 						<img src="/uploads/<?php echo end(explode('/', wp_get_attachment_image_src(get_post_thumbnail_id(), 'post-thumbnail')[0])); ?>">
