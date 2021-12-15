db6d930358fe1d43d11255a8a1158fb2f7773496
nullstep <me@nullstep.com>
2021-12-15 10:11:22 +0000

    bootstrap 5 nav fix

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 	<div id="nav-area" class="content">
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow" role="navigation">
-			<div class="<?php echo getvalue('container_class'); ?> px-0">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-collapse" aria-controls="nav-collapse" aria-expanded="false" aria-label="Toggle Navigation">
+			<div class="<?php echo getvalue('container_class'); ?>">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle Navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 <?php
@@ -13,19 +13,18 @@
 <?php
 	}
 ?>
-				<?php wp_nav_menu([
-					'theme_location' => 'primary',
-					'item_spacing' => 'discard',
-					'depth' => 2,
-					'container' => 'div',
-					'container_class' => 'collapse navbar-collapse',
-					'container_id' => 'nav-collapse',
-					'menu_class' => 'nav navbar-nav mr-auto',
-					'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-					'walker' => new WP_Bootstrap_Navwalker()
-				]);
+				<div id="navbar" class="navbar-collapse collapse">
+					<?php wp_nav_menu([
+						'theme_location' => 'primary',
+						'depth' => 5,
+						'container' => false,
+						'menu_class' => 'navbar-nav mx-auto position-relative',
+						'fallback_cb' => '__return_false',
+						'walker' => new WP_Bootstrap_Navwalker()
+					]);
 ?>
 
+				</div>
 			</div>
 		</nav>
 	</div>
