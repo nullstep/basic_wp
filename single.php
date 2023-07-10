ed1e71a8fb2111cf5b98207d9ecad03c9550c303
nullstep <me@nullstep.com>
2023-07-10 14:53:13 +0100

    fix tags

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 		<div class="row">
-			<article class="col-sm-12">
+			<div class="col-sm-12">
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 					<h2 class="post-title"><?php the_title(); ?></h2>
 					<p><span class="post-date"><?php the_time(get_option('date_format')); ?></span> - <span class="post-time"><?php the_time(); ?></span></p>
@@ -11,6 +11,6 @@
 <?php else: ?>
 					<h2 class="post-title">No Content</h2>
 <?php endif; ?>
-			</article>
+			</div>
 		</div>
 <?php get_footer(); ?>
