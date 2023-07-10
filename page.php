5cb4703ed24c4109b7e8f727fbf9e371027961db
nullstep <me@nullstep.com>
2023-07-10 14:53:28 +0100

    fix tags

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 			<div class="row">
-				<article class="col-sm-12">
+				<div class="col-sm-12">
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 <?php the_content(); ?>
 <?php endwhile; ?>
 <?php else: ?>
 					<h2 class="post-title">No Content</h2>
 <?php endif; ?>
-				</article>
+				</div>
 			</div>
 <?php get_footer(); ?>
