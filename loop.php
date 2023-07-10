04d92293a33ca98a0e484e3ebef0fd42f388f0bd
nullstep <me@nullstep.com>
2023-07-10 14:53:58 +0100

    fix tags

--- loop.php
+++ loop.php
@@ -1,6 +1,6 @@
 				<div class="row feed">
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-					<article class="col-md-3 ">
+					<div class="col-md-3 ">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 							<h2 class="post-title"><?php the_title(); ?></h2>
 							<p><span class="post-date"><?php the_time(get_option('date_format')); ?></span> - <span class="post-time"><?php the_time(); ?></span></p>
@@ -13,11 +13,11 @@
 
 						</p>
 						<p><a class="btn btn-primary" href="<?php the_permalink(); ?>">Read Article&hellip;</a></p>
-					</article>
+					</div>
 <?php endwhile; ?>
 <?php else: ?>
-					<article>
+					<div>
 						<h2 class="post-title">No Articles</h2>
-					</article>
+					</div>
 <?php endif; ?>
 				</div>
