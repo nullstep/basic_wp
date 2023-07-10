d633058a9005cf2c83d0c8056503f61f7ef00b4c
nullstep <me@nullstep.com>
2023-07-10 14:53:44 +0100

    fix nav

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 	<div id="nav-area" class="<?php BWP::nav('sticky'); ?>">
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark<?php BWP::nav('shadow'); ?>" role="navigation">
+		<nav class="navbar navbar-expand-xl navbar-dark bg-dark<?php BWP::nav('shadow'); ?>">
 			<div class="<?php BWP::nav('width'); ?>">
 				<?php BWP::nav(); ?>
 			</div>
