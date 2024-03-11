a6ff88c3879b49887e02adb6a33fbae367f17e0c
nullstep <me@nullstep.com>
2024-03-11 14:05:43 +0000

    fixes, new updater

--- functions.php
+++ functions.php
@@ -921,141 +921,6 @@ class _themeMenu {
 	}
 }
 
-//  ███    █▄      ▄███████▄  ████████▄      ▄████████      ███         ▄████████  
-//  ███    ███    ███    ███  ███   ▀███    ███    ███  ▀█████████▄    ███    ███  
-//  ███    ███    ███    ███  ███    ███    ███    ███     ▀███▀▀██    ███    █▀   
-//  ███    ███    ███    ███  ███    ███    ███    ███      ███   ▀   ▄███▄▄▄      
-//  ███    ███  ▀█████████▀   ███    ███  ▀███████████      ███      ▀▀███▀▀▀      
-//  ███    ███    ███         ███    ███    ███    ███      ███        ███    █▄   
-//  ███    ███    ███         ███   ▄███    ███    ███      ███        ███    ███  
-//  ████████▀    ▄████▀       ████████▀     ███    █▀      ▄████▀      ██████████
-
-class _themeUpdater {
-	protected $theme = _THEME;
-	protected $repository = 'nullstep/' . _THEME;
-	protected $domain = 'https://github.com/';
-	protected $raw_domain = 'https://raw.githubusercontent.com/';
-	protected $css_endpoint = '/main/style.css';
-	protected $zip_endpoint = '/releases/download/v';
-	protected $remote_css_uri;
-	protected $remote_zip_uri;
-	protected $remote_version;
-	protected $local_version;
-
-	public function init() {
-		add_filter('auto_update_theme', [
-			$this,
-			'auto_update_theme'
-		], 20, 2);
-		add_filter('upgrader_source_selection', [
-			$this,
-			'upgrader_source_selection'
-		], 10, 4);
-		add_filter('pre_set_site_transient_update_themes', [
-			$this,
-			'pre_set_site_transient_update_themes'
-		]);
-	}
-
-	public function auto_update_theme($update, $item) {
-		return $this->theme === $item->theme;
-	}
-
-	public function upgrader_source_selection($source, $remote_source, $upgrader, $hook_extra) {
-		global $wp_filesystem;
-
-		$update = [
-			'update-selected',
-			'update-selected-themes',
-			'upgrade-theme'
-		];
-
-		if (!isset($_GET['action']) || !in_array($_GET['action'], $update, TRUE)) {
-			return $source;
-		}
-
-		if (!isset($source, $remote_source)) {
-			return $source;
-		}
-
-		if (FALSE === stristr(basename($source), $this->theme)) {
-			return $source;
-		}
-
-		$basename = basename($source);
-		$upgrader->skin->feedback(esc_html_e('Renaming theme directory.', 'bootstrap'));
-		$corrected_source = str_replace($basename, $this->theme, $source);
-
-		if ($wp_filesystem->move($source, $corrected_source, TRUE)) {
-			$upgrader->skin->feedback(esc_html_e('Rename successful.', 'bootstrap'));
-			return $corrected_source;
-		}
-
-		return new WP_Error();
-	}
-
-	public function pre_set_site_transient_update_themes($transient) {
-		require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-		$this->local_version = (wp_get_theme($this->theme))->get('Version');
-
-		if ($this->has_update()) {
-			$response = [
-				'theme' => $this->theme,
-				'new_version' => $this->remote_version,
-				'url' => $this->construct_repository_uri(),
-				'package' => $this->construct_remote_zip_uri(),
-				'branch' => 'master'
-			];
-			$transient->response[$this->theme] = $response;
-		}
-
-		return $transient;
-	}
-
-	protected function construct_remote_stylesheet_uri() {
-		return $this->remote_css_uri = $this->raw_domain . $this->repository . $this->css_endpoint;
-	}
-
-	protected function construct_remote_zip_uri() {
-		return $this->remote_zip_uri = $this->domain . $this->repository . $this->zip_endpoint . $this->remote_version . '/' . $this->theme . '.zip';
-	}
-
-	protected function construct_repository_uri() {
-		return $this->repository_uri = $this->domain . trailingslashit($this->repository);
-	}
-
-	protected function get_remote_version() {
-		$this->remote_stylesheet_uri = $this->construct_remote_stylesheet_uri();
-		$response = $this->remote_get($this->remote_stylesheet_uri);
-		$response = str_replace("\r", "\n", wp_remote_retrieve_body($response));
-		$headers = [
-			'Version' => 'Version'
-		];
-
-		foreach ($headers as $field => $regex) {
-			if (preg_match('/^[ \t\/*#@]*' . preg_quote($regex, '/') . ':(.*)$/mi', $response, $match) && $match[1]) {
-				$headers[$field] = _cleanup_header_comment($match[1]);
-			}
-			else {
-				$headers[$field] = '';
-			}
-		}
-
-		return $this->remote_version = $headers['Version'];
-	}
-
-	protected function has_update() {
-		if (!$this->remote_version) {
-			$this->remote_version = $this->get_remote_version();
-		}
-		return version_compare($this->remote_version, $this->local_version, '>');
-	}
-
-	protected function remote_get($url, $args = []) {
-		return wp_remote_get($url, $args);
-	}
-}
-
 //  ▀█████████▄   
 //    ███    ███  
 //    ███    ███  
@@ -2032,6 +1897,172 @@ function curl($url) {
 	return $response;
 }
 
+//  ███    █▄      ▄███████▄  ████████▄      ▄████████      ███         ▄████████  
+//  ███    ███    ███    ███  ███   ▀███    ███    ███  ▀█████████▄    ███    ███  
+//  ███    ███    ███    ███  ███    ███    ███    ███     ▀███▀▀██    ███    █▀   
+//  ███    ███    ███    ███  ███    ███    ███    ███      ███   ▀   ▄███▄▄▄      
+//  ███    ███  ▀█████████▀   ███    ███  ▀███████████      ███      ▀▀███▀▀▀      
+//  ███    ███    ███         ███    ███    ███    ███      ███        ███    █▄   
+//  ███    ███    ███         ███   ▄███    ███    ███      ███        ███    ███  
+//  ████████▀    ▄████▀       ████████▀     ███    █▀      ▄████▀      ██████████
+
+if (!class_exists('WPU')) {
+	class WPU {
+		private $file;
+		private $plugin;
+		private $basename;
+		private $active;
+		private $username;
+		private $repository;
+		private $authorize_token;
+		private $github_response;
+
+		public $requires;
+		public $tested;
+
+		public function __construct($file) {
+			$this->file = $file;
+			add_action('admin_init', [$this, 'set_plugin_properties']);
+
+			return $this;
+		}
+
+		public function set_plugin_properties() {
+			$this->plugin = get_plugin_data($this->file);
+			$this->basename = plugin_basename($this->file);
+			$this->active = is_plugin_active($this->basename);
+		}
+
+		public function set_username($username) {
+			$this->username = $username;
+		}
+
+		public function set_repository($repository) {
+			$this->repository = $repository;
+		}
+
+		public function authorize($token) {
+			$this->authorize_token = $token;
+		}
+
+		private function get_repository_info() {
+			if (is_null($this->github_response)) {
+				$request_uri = sprintf('https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository);
+
+				$curl = curl_init();
+
+				curl_setopt_array($curl, [
+					CURLOPT_URL => $request_uri,
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => '',
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 0,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => 'GET',
+					CURLOPT_HTTPHEADER => [
+						'Authorization: token ' . $this->authorize_token,
+						'User-Agent: WPUpdater/1.0.0'
+					]
+				]);
+
+				$response = curl_exec($curl);
+
+				curl_close($curl);
+
+				$response = json_decode($response, true);
+
+				if (is_array($response)) {
+					$response = current($response);
+				}
+
+				$this->github_response = $response;
+			}
+		}
+
+		public function initialize() {
+			add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
+			add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
+			add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
+		}
+
+		public function modify_transient($transient) {
+			if (property_exists($transient, 'checked')) {
+				if ($checked = $transient->checked) {
+					$this->get_repository_info();
+
+					$out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
+
+					if ($out_of_date) {
+						$new_files = $this->github_response['zipball_url'];
+						$slug = current(explode('/', $this->basename));
+
+						$plugin = [
+							'url' => $this->plugin['PluginURI'],
+							'slug' => $slug,
+							'package' => $new_files,
+							'new_version' => $this->github_response['tag_name']
+						];
+
+						$transient->response[$this->basename] = (object) $plugin;
+					}
+				}
+			}
+
+			return $transient;
+		}
+
+		public function plugin_popup($result, $action, $args) {
+			if ($action !== 'plugin_information') {
+				return false;
+			}
+
+			if (!empty($args->slug)) {
+				if ($args->slug == current(explode('/' , $this->basename))) {
+					$this->get_repository_info();
+
+					$plugin = [
+						'name' => $this->plugin['Name'],
+						'slug' => $this->basename,
+						'requires' => $this->$requires ?? '6.3',
+						'tested' => $this->$tested ?? '6.4.3',
+						'version' => $this->github_response['tag_name'],
+						'author' => $this->plugin['AuthorName'],
+						'author_profile' => $this->plugin['AuthorURI'],
+						'last_updated' => $this->github_response['published_at'],
+						'homepage' => $this->plugin['PluginURI'],
+						'short_description' => $this->plugin['Description'],
+						'sections' => [
+							'Description' => $this->plugin['Description'],
+							'Updates' => $this->github_response['body'],
+						],
+						'download_link' => $this->github_response['zipball_url']
+					];
+
+					return (object) $plugin;
+				}
+			}
+
+
+			return $result;
+		}
+
+		public function after_install($response, $hook_extra, $result) {
+			global $wp_filesystem;
+
+			$install_directory = plugin_dir_path($this->file);
+			$wp_filesystem->move($result['destination'], $install_directory);
+			$result['destination'] = $install_directory;
+
+			if ($this->active) {
+				activate_plugin($this->basename);
+			}
+
+			return $result;
+		}
+	}
+}
+
 //   ▄█   ███▄▄▄▄▄     ▄█       ███      
 //  ███   ███▀▀▀▀██▄  ███   ▀█████████▄  
 //  ███▌  ███    ███  ███▌     ▀███▀▀██  
@@ -2041,10 +2072,17 @@ function curl($url) {
 //  ███   ███    ███  ███       ███      
 //  █▀     ▀█    █▀   █▀       ▄████▀
 
-// updater
+// init updater
 
-//$updater = new _themeUpdater();
-//$updater->init();
+if (get_option('auth_key') !== '') {
+	$updater = new WPU(__FILE__);
+	$updater->$requires = '6.4';
+	$updater->$tested = '6.4.3';
+	$updater->set_username('nullstep');
+	$updater->set_repository('basic_wp');
+	$updater->authorize(get_option('auth_key'));
+	$updater->initialize();
+}
 
 // oh mah gawd a global!
 
