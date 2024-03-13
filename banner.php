cba484cfd2a2c6bfc85e880f33513b829d6ea5ec
nullstep <me@nullstep.com>
2024-03-13 00:44:49 +0000

    banner fixes

--- banner.php
+++ banner.php
@@ -1,9 +1,10 @@
 	<header id="banner-area" class="py-0">
 <?php if (is_front_page()): ?>
 		<div id="home-banner-image">
-			<div class="<?php B::value('container_class'); ?>"<?php B::featured('bg'); ?>>
-				<div class="row">
-					<div class="col-xs-12 text-center">
+			<div class="<?php B::value('container_class'); ?> relative">
+				<?php B::featured('img'); ?>
+				<div class="banner-box">
+					<div class="text-center mt-5">
 						<h1><?php the_title(); ?></h1>
 					</div>
 				</div>
